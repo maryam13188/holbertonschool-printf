370a9931ccbc59be5b2e3117f3c2cb0b8402154a
munirahenad <muneraenad@hotmail.com>
2025-11-17 11:04:26 +0000

    Add basic _printf implementation supporting %d and %i
    - Implement simplified _printf handling integer specifiers %d and %i
    - Add recursive integer printing with print_integer and print_unsigned
    - Handle %% for printing literal percent
    - Add input validation and character counting
    - No flags, width, precision, or length modifiers per task requirements

--- _printf.c
+++ _printf.c
@@ -1,95 +1,108 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
- * _printf - produces output according to a format
- * @format: character string containing directives
+ * print_unsigned - prints an unsigned integer using _putchar
+ * @num: number to print
  *
- * Return: number of characters printed (excluding null byte)
+ * Return: number of printed characters
+ */
+static int print_unsigned(unsigned int num)
+{
+	int count = 0;
+
+	if (num / 10)
+		count += print_unsigned(num / 10);
+
+	_putchar((num % 10) + '0');
+	count++;
+
+	return (count);
+}
+
+/**
+ * print_integer - prints an integer using _putchar
+ * @n: integer to print
+ *
+ * Return: number of printed characters
+ */
+static int print_integer(int n)
+{
+	unsigned int num;
+	int count = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = (unsigned int)(-n);
+	}
+	else
+	{
+		num = (unsigned int)n;
+	}
+
+	count += print_unsigned(num);
+
+	return (count);
+}
+
+/**
+ * _printf - simplified printf that handles %d and %i
+ * @format: format string
+ *
+ * Return: number of printed characters, or -1 on error
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
+	va_list ap;
+	int i = 0;
 	int count = 0;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
-	count = process_format(format, args);
-	va_end(args);
+	va_start(ap, format);
 
-	return (count);
-}
-
-/**
- * process_format - process the format string
- * @format: format string to process
- * @args: variable arguments list
- *
- * Return: number of characters printed
- */
-int process_format(const char *format, va_list args)
-{
-	int count = 0, i = 0;
-	int specifier_count;
-
-	while (format[i])
+	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			specifier_count = handle_specifier(format, &i, args);
-			if (specifier_count == -1)
-				return (-1);
-			count += specifier_count;
+			_putchar(format[i]);
+			count++;
 		}
 		else
 		{
-			count += write_char(format[i]);
+			i++;
+
+			if (format[i] == '\0')
+			{
+				va_end(ap);
+				return (-1);
+			}
+
+			if (format[i] == 'd' || format[i] == 'i')
+			{
+				int n = va_arg(ap, int);
+
+				count += print_integer(n);
+			}
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
 		}
 		i++;
 	}
+
+	va_end(ap);
 	return (count);
 }
 
-/**
- * handle_specifier - handle format specifiers
- * @format: format string
- * @i: pointer to current index
- * @args: variable arguments list
- *
- * Return: number of characters printed for specifier
- */
-int handle_specifier(const char *format, int *i, va_list args)
-{
-	int count = 0;
-
-	(*i)++;
-	if (format[*i] == '\0')
-		return (-1);
-
-	if (format[*i] == 'c')
-		count = print_char(args);
-	else if (format[*i] == 's')
-		count = print_string(args);
-	else if (format[*i] == '%')
-		count = print_percent(args);
-	else
-	{
-		write(1, "%", 1);
-		write(1, &format[*i], 1);
-		count = 2;
-	}
-
-	return (count);
-}
-
-/**
- * write_char - write a single character
- * @c: character to write
- *
- * Return: 1 (number of characters written)
- */
-int write_char(char c)
-{
-	return (write(1, &c, 1));
-}
