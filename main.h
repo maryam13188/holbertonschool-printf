00ea9224e1c972414e5227d684e7afec9ba5e133
munirahenad <muneraenad@hotmail.com>
2025-11-17 11:29:35 +0000

    feat: add int function prototypes for d and i specifiers
    - Add print_int and print_number function prototypes
    - Prepare header for integer conversion support

--- main.h
+++ main.h
@@ -9,13 +9,10 @@ int _printf(const char *format, ...);
 int process_format(const char *format, va_list args);
 int handle_specifier(const char *format, int *i, va_list args);
 int write_char(char c);
-int write_specifier(char c);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
-
-
-int _putchar(char c);
+int print_int(va_list args);
+int print_number(int n);
 
 #endif /* MAIN_H */
-
