479cfdf6239b9b7392e5b55ac93ef7e4e0c32f8c
maryam13188 <roro13188@gmail.com>
2025-11-17 16:38:40 +0000

    refactor: adapt all print functions for buffer system

--- print_functions.c
+++ print_functions.c
@@ -3,166 +3,176 @@
 /**
  * print_char - prints a character
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed (1)
  */
-int print_char(va_list args)
+int print_char(va_list args, char buffer[], int *buff_ind)
 {
-char c = va_arg(args, int);
-
-return (write(1, &c, 1));
+    char c = va_arg(args, int);
+    return (buffer_char(c, buffer, buff_ind));
 }
 
 /**
  * print_string - prints a string
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_string(va_list args)
+int print_string(va_list args, char buffer[], int *buff_ind)
 {
-char *str = va_arg(args, char *);
-int count = 0;
-
-if (str == NULL)
-str = "(null)";
-
-while (str[count])
-{
-write(1, &str[count], 1);
-count++;
-}
-
-return (count);
+    char *str = va_arg(args, char *);
+    return (buffer_string(str, buffer, buff_ind));
 }
 
 /**
  * print_percent - prints a percent sign
  * @args: arguments list (unused)
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed (1)
  */
-int print_percent(va_list args)
+int print_percent(va_list args, char buffer[], int *buff_ind)
 {
-(void)args;
-return (write(1, "%", 1));
+    (void)args;
+    return (buffer_char('%', buffer, buff_ind));
 }
 
 /**
  * print_int - prints an integer
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_int(va_list args)
+int print_int(va_list args, char buffer[], int *buff_ind)
 {
-int n = va_arg(args, int);
-return (print_number(n));
+    int n = va_arg(args, int);
+    return (print_number(n, buffer, buff_ind));
 }
 
 /**
  * print_number - prints a number recursively
  * @n: number to print
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_number(int n)
+int print_number(int n, char buffer[], int *buff_ind)
 {
-int count = 0;
-unsigned int num;
+    int count = 0;
+    unsigned int num;
 
-if (n < 0)
-{
-count += write(1, "-", 1);
-num = -n;
-}
-else
-{
-num = n;
-}
+    if (n < 0)
+    {
+        count += buffer_char('-', buffer, buff_ind);
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
 
-if (num / 10)
-count += print_number(num / 10);
+    if (num / 10)
+        count += print_number(num / 10, buffer, buff_ind);
 
-count += write(1, &"0123456789"[num % 10], 1);
-return (count);
+    count += buffer_char("0123456789"[num % 10], buffer, buff_ind);
+    return (count);
 }
 
 /**
  * print_binary - prints an unsigned int in binary
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_binary(va_list args)
+int print_binary(va_list args, char buffer[], int *buff_ind)
 {
-unsigned int n = va_arg(args, unsigned int);
-return (print_binary_recursive(n));
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_binary_recursive(n, buffer, buff_ind));
 }
 
 /**
  * print_binary_recursive - prints binary recursively
  * @n: number to print in binary
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_binary_recursive(unsigned int n)
+int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind)
 {
-int count = 0;
+    int count = 0;
 
-if (n / 2)
-count += print_binary_recursive(n / 2);
+    if (n / 2)
+        count += print_binary_recursive(n / 2, buffer, buff_ind);
 
-count += write(1, &"01"[n % 2], 1);
-return (count);
+    count += buffer_char("01"[n % 2], buffer, buff_ind);
+    return (count);
 }
 
 /**
  * print_unsigned - prints an unsigned integer
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_unsigned(va_list args)
+int print_unsigned(va_list args, char buffer[], int *buff_ind)
 {
-unsigned int n = va_arg(args, unsigned int);
-return (print_unsigned_number(n, 10, "0123456789"));
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_unsigned_number(n, 10, "0123456789", buffer, buff_ind));
 }
 
 /**
  * print_octal - prints an unsigned integer in octal
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_octal(va_list args)
+int print_octal(va_list args, char buffer[], int *buff_ind)
 {
-unsigned int n = va_arg(args, unsigned int);
-return (print_unsigned_number(n, 8, "01234567"));
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_unsigned_number(n, 8, "01234567", buffer, buff_ind));
 }
 
 /**
  * print_hex_lower - prints an unsigned integer in hexadecimal (lowercase)
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_hex_lower(va_list args)
+int print_hex_lower(va_list args, char buffer[], int *buff_ind)
 {
-unsigned int n = va_arg(args, unsigned int);
-return (print_unsigned_number(n, 16, "0123456789abcdef"));
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_unsigned_number(n, 16, "0123456789abcdef", buffer, buff_ind));
 }
 
 /**
  * print_hex_upper - prints an unsigned integer in hexadecimal (uppercase)
  * @args: arguments list
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_hex_upper(va_list args)
+int print_hex_upper(va_list args, char buffer[], int *buff_ind)
 {
-unsigned int n = va_arg(args, unsigned int);
-return (print_unsigned_number(n, 16, "0123456789ABCDEF"));
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_unsigned_number(n, 16, "0123456789ABCDEF", buffer, buff_ind));
 }
 
 /**
@@ -170,16 +180,18 @@ return (print_unsigned_number(n, 16, "0123456789ABCDEF"));
  * @n: number to print
  * @base: base for conversion (8, 10, 16)
  * @digits: digits to use for the base
+ * @buffer: character buffer
+ * @buff_ind: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int print_unsigned_number(unsigned int n, unsigned int base, const char *digits)
+int print_unsigned_number(unsigned int n, unsigned int base, const char *digits, char buffer[], int *buff_ind)
 {
-int count = 0;
+    int count = 0;
 
-if (n / base)
-count += print_unsigned_number(n / base, base, digits);
+    if (n / base)
+        count += print_unsigned_number(n / base, base, digits, buffer, buff_ind);
 
-count += write(1, &digits[n % base], 1);
-return (count);
+    count += buffer_char(digits[n % base], buffer, buff_ind);
+    return (count);
 }
