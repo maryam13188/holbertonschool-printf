9039dd2392ac12ed36b822c4c4d5e9e31f0aca68
munirahenad <muneraenad@hotmail.com>
2025-11-17 16:24:14 +0000

    feat: implement unsigned number conversion function

--- print_functions.c
+++ print_functions.c
@@ -8,9 +8,9 @@
  */
 int print_char(va_list args)
 {
-	char c = va_arg(args, int);
+char c = va_arg(args, int);
 
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
@@ -21,19 +21,19 @@ int print_char(va_list args)
  */
 int print_string(va_list args)
 {
-	char *str = va_arg(args, char *);
-	int count = 0;
+char *str = va_arg(args, char *);
+int count = 0;
 
-	if (str == NULL)
-		str = "(null)";
+if (str == NULL)
+str = "(null)";
 
-	while (str[count])
-	{
-		write(1, &str[count], 1);
-		count++;
-	}
+while (str[count])
+{
+write(1, &str[count], 1);
+count++;
+}
 
-	return (count);
+return (count);
 }
 
 /**
@@ -44,8 +44,8 @@ int print_string(va_list args)
  */
 int print_percent(va_list args)
 {
-	(void)args;
-	return (write(1, "%", 1));
+(void)args;
+return (write(1, "%", 1));
 }
 
 /**
@@ -56,8 +56,8 @@ int print_percent(va_list args)
  */
 int print_int(va_list args)
 {
-	int n = va_arg(args, int);
-	return (print_number(n));
+int n = va_arg(args, int);
+return (print_number(n));
 }
 
 /**
@@ -68,24 +68,24 @@ int print_int(va_list args)
  */
 int print_number(int n)
 {
-	int count = 0;
-	unsigned int num;
+int count = 0;
+unsigned int num;
 
-	if (n < 0)
-	{
-		count += write(1, "-", 1);
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
+if (n < 0)
+{
+count += write(1, "-", 1);
+num = -n;
+}
+else
+{
+num = n;
+}
 
-	if (num / 10)
-		count += print_number(num / 10);
+if (num / 10)
+count += print_number(num / 10);
 
-	count += write(1, &"0123456789"[num % 10], 1);
-	return (count);
+count += write(1, &"0123456789"[num % 10], 1);
+return (count);
 }
 
 /**
@@ -96,8 +96,8 @@ int print_number(int n)
  */
 int print_binary(va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int);
-	return (print_binary_recursive(n));
+unsigned int n = va_arg(args, unsigned int);
+return (print_binary_recursive(n));
 }
 
 /**
@@ -108,11 +108,78 @@ int print_binary(va_list args)
  */
 int print_binary_recursive(unsigned int n)
 {
-	int count = 0;
+int count = 0;
 
-	if (n / 2)
-		count += print_binary_recursive(n / 2);
+if (n / 2)
+count += print_binary_recursive(n / 2);
 
-	count += write(1, &"01"[n % 2], 1);
-	return (count);
+count += write(1, &"01"[n % 2], 1);
+return (count);
+}
+
+/**
+ * print_unsigned - prints an unsigned integer
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int print_unsigned(va_list args)
+{
+unsigned int n = va_arg(args, unsigned int);
+return (print_unsigned_number(n, 10, "0123456789"));
+}
+
+/**
+ * print_octal - prints an unsigned integer in octal
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int print_octal(va_list args)
+{
+unsigned int n = va_arg(args, unsigned int);
+return (print_unsigned_number(n, 8, "01234567"));
+}
+
+/**
+ * print_hex_lower - prints an unsigned integer in hexadecimal (lowercase)
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int print_hex_lower(va_list args)
+{
+unsigned int n = va_arg(args, unsigned int);
+return (print_unsigned_number(n, 16, "0123456789abcdef"));
+}
+
+/**
+ * print_hex_upper - prints an unsigned integer in hexadecimal (uppercase)
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int print_hex_upper(va_list args)
+{
+unsigned int n = va_arg(args, unsigned int);
+return (print_unsigned_number(n, 16, "0123456789ABCDEF"));
+}
+
+/**
+ * print_unsigned_number - prints unsigned number in given base
+ * @n: number to print
+ * @base: base for conversion (8, 10, 16)
+ * @digits: digits to use for the base
+ *
+ * Return: number of characters printed
+ */
+int print_unsigned_number(unsigned int n, unsigned int base, const char *digits)
+{
+int count = 0;
+
+if (n / base)
+count += print_unsigned_number(n / base, base, digits);
+
+count += write(1, &digits[n % base], 1);
+return (count);
 }
