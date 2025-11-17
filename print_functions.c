abe5e74c25c3131cb2c879fd0196233210d3124c
munirahenad <muneraenad@hotmail.com>
2025-11-17 12:38:17 +0000

    feat: add d and i specifiers with helper functions
    - Implement print_int and print_number for integer handling
    - Maintain existing c, s, and % functionality
    - Support negative numbers and accurate character counting

--- print_functions.c
+++ print_functions.c
@@ -8,9 +8,9 @@
  */
 int print_char(va_list args)
 {
-	char c = va_arg(args, int);
+char c = va_arg(args, int);
 
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
@@ -21,19 +21,19 @@ int print_char(va_list args)
  */
 int print_string(va_list args)
 {
-	char *str = va_arg(args, char *);
-	int count = 0;
+char *str = va_arg(args, char *);
+int count = 0;
 
-	if (str == NULL)
-		str = "(null)";
+if (str == NULL)
+str = "(null)";
 
-	while (str[count])
-	{
-		write(1, &str[count], 1);
-		count++;
-	}
+while (str[count])
+{
+write(1, &str[count], 1);
+count++;
+}
 
-	return (count);
+return (count);
 }
 
 /**
@@ -44,6 +44,46 @@ int print_string(va_list args)
  */
 int print_percent(va_list args)
 {
-	(void)args;
-	return (write(1, "%", 1));
+(void)args;
+return (write(1, "%", 1));
+}
+
+/**
+ * print_int - prints an integer
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int print_int(va_list args)
+{
+int n = va_arg(args, int);
+return (print_number(n));
+}
+
+/**
+ * print_number - prints a number recursively
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_number(int n)
+{
+int count = 0;
+unsigned int num;
+
+if (n < 0)
+{
+count += write(1, "-", 1);
+num = -n;
+}
+else
+{
+num = n;
+}
+
+if (num / 10)
+count += print_number(num / 10);
+
+count += write(1, &"0123456789"[num % 10], 1);
+return (count);
 }
