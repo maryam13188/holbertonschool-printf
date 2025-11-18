fa7241f39e9a8ef858c1a92c7c8d465acaa60c2b
maryam13188 <roro13188@gmail.com>
2025-11-18 10:04:40 +0000

    feat: add pointer p specifier function prototypes

--- main.h
+++ main.h
@@ -7,23 +7,9 @@
 
 #define BUFFER_SIZE 1024
 
-/* Flag characters */
-#define FLAG_PLUS   1
-#define FLAG_SPACE  2
-#define FLAG_HASH   4
-
-typedef struct format_info
-{
-    int flags;
-    int width;
-    int precision;
-    int length;
-} format_info_t;
-
 int _printf(const char *format, ...);
 int process_format(const char *format, va_list args, char buffer[], int *buff_ind);
 int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
-int parse_flags(const char *format, int *i, format_info_t *info);
 int flush_buffer(char buffer[], int *buff_ind);
 int buffer_char(char c, char buffer[], int *buff_ind);
 int buffer_string(char *str, char buffer[], int *buff_ind);
@@ -43,21 +29,5 @@ int print_hex_upper(va_list args, char buffer[], int *buff_ind);
 int print_unsigned_number(unsigned int n, unsigned int base, const char *digits, char buffer[], int *buff_ind);
 int print_hex_byte(unsigned char c, char buffer[], int *buff_ind);
 int print_pointer_address(void *ptr, char buffer[], int *buff_ind);
-int print_char(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_string(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_custom_string(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_pointer(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_percent(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_int(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_number(int n, char buffer[], int *buff_ind, format_info_t info);
-int print_binary(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind, format_info_t info);
-int print_unsigned(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_octal(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_hex_lower(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_hex_upper(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_unsigned_number(unsigned int n, unsigned int base, const char *digits, char buffer[], int *buff_ind, format_info_t info);
-int print_hex_byte(unsigned char c, char buffer[], int *buff_ind, format_info_t info);
-int print_pointer_address(void *ptr, char buffer[], int *buff_ind, format_info_t info);
 
 #endif /* MAIN_H */
