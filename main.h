a6fdf9a409f80fa5784db7890e305b51b53b18f7
maryam13188 <roro13188@gmail.com>
2025-11-18 19:52:34 +0000

    feat: add field width support infrastructure for task 10

--- main.h
+++ main.h
@@ -21,6 +21,7 @@ typedef struct format_info
 {
     int flags;
     int length;
+    int width;
 } format_info_t;
 
 /* Main printf function */
@@ -40,12 +41,16 @@ int print_percent(va_list args, char buffer[], int *buff_ind, format_info_t info
 int print_int(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_number(long n, char buffer[], int *buff_ind, format_info_t info);
 int print_binary(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind, format_info_t info); /* Added prototype */
+int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind, format_info_t info);
 int print_unsigned(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_octal(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_hex_lower(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_hex_upper(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_unsigned_number(unsigned long n, unsigned int base, const char *digits, char buffer[], int *buff_ind, format_info_t info);
+
+/* Field width functions */
+int apply_field_width(char buffer[], int *buff_ind, format_info_t info, int num_chars, int is_negative);
+int parse_width(const char *format, int *i, va_list args);
 
 /* Helper functions */
 int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
