b3be1f8cf87ec8f1b13cd822f6de6eacf6398a0c
maryam13188 <roro13188@gmail.com>
2025-11-18 18:55:18 +0000

    feat: add header file with all prototypes and flag definitions

--- main.h
+++ main.h
@@ -7,41 +7,42 @@
 
 #define BUFFER_SIZE 1024
 
-/* Flag characters for question 8 */
+/* Flag characters */
 #define FLAG_PLUS   1
 #define FLAG_SPACE  2
 #define FLAG_HASH   4
 
-/* All original functions from questions 0-7 */
+typedef struct format_info
+{
+    int flags;
+} format_info_t;
+
+/* Main printf function */
 int _printf(const char *format, ...);
-int process_format(const char *format, va_list args, char buffer[], int *buff_ind);
-int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
+
+/* Buffer management functions */
 int flush_buffer(char buffer[], int *buff_ind);
 int buffer_char(char c, char buffer[], int *buff_ind);
 int buffer_string(char *str, char buffer[], int *buff_ind);
-int print_char(va_list args, char buffer[], int *buff_ind);
-int print_string(va_list args, char buffer[], int *buff_ind);
-int print_custom_string(va_list args, char buffer[], int *buff_ind);
-int print_pointer(va_list args, char buffer[], int *buff_ind);
-int print_percent(va_list args, char buffer[], int *buff_ind);
-int print_int(va_list args, char buffer[], int *buff_ind);
-int print_number(int n, char buffer[], int *buff_ind);
-int print_binary(va_list args, char buffer[], int *buff_ind);
-int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind);
-int print_unsigned(va_list args, char buffer[], int *buff_ind);
-int print_octal(va_list args, char buffer[], int *buff_ind);
-int print_hex_lower(va_list args, char buffer[], int *buff_ind);
-int print_hex_upper(va_list args, char buffer[], int *buff_ind);
-int print_unsigned_number(unsigned int n, unsigned int base, const char *digits, char buffer[], int *buff_ind);
-int print_hex_byte(unsigned char c, char buffer[], int *buff_ind);
-int print_pointer_address(void *ptr, char buffer[], int *buff_ind);
 
-/* New functions for question 8 - flag support */
-int handle_specifier_with_flags(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
-int parse_flags(const char *format, int *i);
-int print_int_with_flags(va_list args, char buffer[], int *buff_ind, int flags);
-int print_octal_with_flags(va_list args, char buffer[], int *buff_ind, int flags);
-int print_hex_lower_with_flags(va_list args, char buffer[], int *buff_ind, int flags);
-int print_hex_upper_with_flags(va_list args, char buffer[], int *buff_ind, int flags);
+/* Conversion specifier functions */
+int print_char(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_string(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_custom_string(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_pointer(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_percent(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_int(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_number(int n, char buffer[], int *buff_ind, format_info_t info);
+int print_binary(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind, format_info_t info);
+int print_unsigned(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_octal(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_hex_lower(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_hex_upper(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_unsigned_number(unsigned int n, unsigned int base, const char *digits, char buffer[], int *buff_ind, format_info_t info);
+
+/* Helper functions */
+int parse_flags(const char *format, int *i, format_info_t *info);
+int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
