a90903d5c1c5852d0b91955bc7e1fad4e08c3c69
root <root@ip-10-42-34-52.ec2.internal>
2025-11-19 07:46:27 +0000

    feat: add pointer p specifier prototypes

--- main.h
+++ main.h
@@ -4,46 +4,30 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
 
 #define BUFFER_SIZE 1024
 
-/* Length modifiers */
-#define LENGTH_NONE 0
-#define LENGTH_L    1
-#define LENGTH_H    2
-
-typedef struct format_info
-{
-    int length;
-    int precision;
-    int has_precision;
-    int width;
-    int has_width;
-} format_info_t;
-
-/* Main printf function */
 int _printf(const char *format, ...);
-
-/* Buffer management functions */
+int process_format(const char *format, va_list args, char buffer[], int *buff_ind);
+int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
 int flush_buffer(char buffer[], int *buff_ind);
 int buffer_char(char c, char buffer[], int *buff_ind);
 int buffer_string(char *str, char buffer[], int *buff_ind);
-
-/* Conversion specifier functions */
-int print_char(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_string(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_custom_string(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_pointer(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_percent(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_int(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_number(long n, char buffer[], int *buff_ind, format_info_t info);
-int print_number_precision(long n, char buffer[], int *buff_ind, format_info_t info);
-int print_binary(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind, format_info_t info);
-int print_unsigned(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_octal(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_hex_lower(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_hex_upper(va_list args, char buffer[], int *buff_ind, format_info_t info);
+int print_char(va_list args, char buffer[], int *buff_ind);
+int print_string(va_list args, char buffer[], int *buff_ind);
+int print_custom_string(va_list args, char buffer[], int *buff_ind);
+int print_pointer(va_list args, char buffer[], int *buff_ind);
+int print_percent(va_list args, char buffer[], int *buff_ind);
+int print_int(va_list args, char buffer[], int *buff_ind);
+int print_number(int n, char buffer[], int *buff_ind);
+int print_binary(va_list args, char buffer[], int *buff_ind);
+int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind);
+int print_unsigned(va_list args, char buffer[], int *buff_ind);
+int print_octal(va_list args, char buffer[], int *buff_ind);
+int print_hex_lower(va_list args, char buffer[], int *buff_ind);
+int print_hex_upper(va_list args, char buffer[], int *buff_ind);
+int print_unsigned_number(unsigned int n, unsigned int base, const char *digits, char buffer[], int *buff_ind);
+int print_hex_byte(unsigned char c, char buffer[], int *buff_ind);
+int print_pointer_address(void *ptr, char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
