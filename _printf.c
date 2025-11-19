30872631049988a095f70cd152978fd01fe00918
munirahenad <muneraenad@hotmail.com>
2025-11-19 16:38:04 +0000

    feat: complete _printf implementation with full formatting support

--- _printf.c
+++ _printf.c
@@ -1,166 +1,193 @@
 #include "main.h"
+#include <stdarg.h>
+#include <ctype.h>
 
-/**
- * flush_buffer - writes the buffer to stdout
- * @buffer: character buffer
- * @buff_ind: pointer to buffer index
- *
- * Return: number of characters written
- */
-int flush_buffer(char buffer[], int *buff_ind)
+static void init_opts(fmt_options *o)
 {
-    if (*buff_ind > 0)
-    {
-        write(1, buffer, *buff_ind);
-        *buff_ind = 0;
-    }
-    return (0);
+    o->plus = o->space = o->hash = o->zero = o->dash = 0;
+    o->width = 0;
+    o->precision = 0;
+    o->precision_specified = 0;
+    o->length = 0;
 }
 
-/**
- * buffer_char - adds a character to buffer, flushes if full
- * @c: character to add
- * @buffer: character buffer
- * @buff_ind: pointer to buffer index
- *
- * Return: 1 (character was buffered)
- */
-int buffer_char(char c, char buffer[], int *buff_ind)
-{
-    if (*buff_ind == BUFFER_SIZE)
-        flush_buffer(buffer, buff_ind);
-    
-    buffer[*buff_ind] = c;
-    (*buff_ind)++;
-    return (1);
-}
-
-/**
- * buffer_string - adds a string to buffer
- * @str: string to add
- * @buffer: character buffer
- * @buff_ind: pointer to buffer index
- *
- * Return: number of characters buffered
- */
-int buffer_string(char *str, char buffer[], int *buff_ind)
-{
-    int count = 0;
-
-    if (str == NULL)
-        str = "(null)";
-
-    while (str[count])
-    {
-        buffer_char(str[count], buffer, buff_ind);
-        count++;
-    }
-    return (count);
-}
-
-/**
- * handle_specifier - handle format specifiers
- * @format: format string
- * @i: pointer to current index
- * @args: variable arguments list
- * @buffer: character buffer
- * @buff_ind: pointer to buffer index
- *
- * Return: number of characters printed for specifier
- */
-int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind)
-{
-    int count = 0;
-
-    (*i)++;
-    if (format[*i] == '\0')
-        return (-1);
-
-    if (format[*i] == 'c')
-        count = print_char(args, buffer, buff_ind);
-    else if (format[*i] == 's')
-        count = print_string(args, buffer, buff_ind);
-    else if (format[*i] == 'S')
-        count = print_custom_string(args, buffer, buff_ind);
-    else if (format[*i] == 'p')
-        count = print_pointer(args, buffer, buff_ind);
-    else if (format[*i] == '%')
-        count = print_percent(args, buffer, buff_ind);
-    else if (format[*i] == 'd' || format[*i] == 'i')
-        count = print_int(args, buffer, buff_ind);
-    else if (format[*i] == 'b')
-        count = print_binary(args, buffer, buff_ind);
-    else if (format[*i] == 'u')
-        count = print_unsigned(args, buffer, buff_ind);
-    else if (format[*i] == 'o')
-        count = print_octal(args, buffer, buff_ind);
-    else if (format[*i] == 'x')
-        count = print_hex_lower(args, buffer, buff_ind);
-    else if (format[*i] == 'X')
-        count = print_hex_upper(args, buffer, buff_ind);
-    else
-    {
-        buffer_char('%', buffer, buff_ind);
-        buffer_char(format[*i], buffer, buff_ind);
-        count = 2;
-    }
-
-    return (count);
-}
-
-/**
- * process_format - process the format string
- * @format: format string to process
- * @args: variable arguments list
- * @buffer: character buffer
- * @buff_ind: pointer to buffer index
- *
- * Return: number of characters printed
- */
-int process_format(const char *format, va_list args, char buffer[], int *buff_ind)
-{
-    int count = 0, i = 0;
-    int specifier_count;
-
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            specifier_count = handle_specifier(format, &i, args, buffer, buff_ind);
-            if (specifier_count == -1)
-                return (-1);
-            count += specifier_count;
-        }
-        else
-        {
-            buffer_char(format[i], buffer, buff_ind);
-            count++;
-        }
-        i++;
-    }
-    return (count);
-}
-
-/**
- * _printf - produces output according to a format
- * @format: character string containing directives
- *
- * Return: number of characters printed (excluding null byte)
- */
 int _printf(const char *format, ...)
 {
     va_list args;
-    char buffer[BUFFER_SIZE];
-    int buff_ind = 0;
     int count = 0;
+    fmt_options opts;
 
-    if (format == NULL)
+    if (!format)
         return (-1);
 
     va_start(args, format);
-    count = process_format(format, args, buffer, &buff_ind);
-    flush_buffer(buffer, &buff_ind); /* Flush any remaining characters */
-    va_end(args);
 
-    return (count);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            init_opts(&opts);
+
+            /* parse flags */
+            while (*format == '+' || *format == ' ' || *format == '#' ||
+                   *format == '0' || *format == '-')
+            {
+                if (*format == '+')
+                    opts.plus = 1;
+                else if (*format == ' ')
+                    opts.space = 1;
+                else if (*format == '#')
+                    opts.hash = 1;
+                else if (*format == '0')
+                    opts.zero = 1;
+                else if (*format == '-')
+                {
+                    opts.dash = 1;
+                    /* '-' overrides '0' */
+                    opts.zero = 0;
+                }
+                format++;
+            }
+
+            /* parse width (number or *) */
+            if (*format == '*')
+            {
+                int w = va_arg(args, int);
+                if (w < 0)
+                {
+                    opts.dash = 1;
+                    opts.width = -w;
+                }
+                else
+                {
+                    opts.width = w;
+                }
+                format++;
+            }
+            else if (isdigit((unsigned char)*format))
+            {
+                opts.width = 0;
+                while (isdigit((unsigned char)*format))
+                {
+                    opts.width = opts.width * 10 + (*format - '0');
+                    format++;
+                }
+            }
+
+            /* parse precision ('.' followed by number or '*') */
+            if (*format == '.')
+            {
+                format++;
+                opts.precision_specified = 1;
+                if (*format == '*')
+                {
+                    int p = va_arg(args, int);
+                    if (p >= 0)
+                    {
+                        opts.precision = p;
+                    }
+                    else
+                    {
+                        /* negative precision means it's ignored */
+                        opts.precision_specified = 0;
+                        opts.precision = 0;
+                    }
+                    format++;
+                }
+                else
+                {
+                    opts.precision = 0;
+                    while (isdigit((unsigned char)*format))
+                    {
+                        opts.precision = opts.precision * 10 + (*format - '0');
+                        format++;
+                    }
+                }
+            }
+
+            /* parse length */
+            if (*format == 'h' || *format == 'l')
+            {
+                opts.length = *format;
+                format++;
+            }
+
+            if (*format == '\0')
+            {
+                va_end(args);
+                return (-1);
+            }
+
+            switch (*format)
+            {
+            case 'c':
+                count += print_char(args, &opts);
+                break;
+
+            case 's':
+                count += print_string(args, &opts);
+                break;
+
+            case '%':
+                count += print_percent(args, &opts);
+                break;
+
+            case 'r': /* custom: reversed string */
+                count += print_reverse(args, &opts);
+                break;
+
+            case 'R': /* custom: ROT13 string */
+                count += print_rot13(args, &opts);
+                break;
+
+            case 'S': /* custom: non-printable as \xHH */
+                count += print_S(args, &opts);
+                break;
+
+            case 'd':
+            case 'i':
+                count += print_number(args, &opts);
+                break;
+
+            case 'u':
+                count += print_unsigned(args, &opts, 10, 0);
+                break;
+
+            case 'o':
+                count += print_unsigned(args, &opts, 8, 0);
+                break;
+
+            case 'x':
+                count += print_unsigned(args, &opts, 16, 0);
+                break;
+
+            case 'X':
+                count += print_unsigned(args, &opts, 16, 1);
+                break;
+
+            case 'p': /* pointer */
+                count += print_pointer(args, &opts);
+                break;
+
+            case 'b': /* custom: binary */
+                count += print_unsigned(args, &opts, 2, 0);
+                break;
+
+            default:
+                count += _putchar('%');
+                count += _putchar(*format);
+                break;
+            }
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
