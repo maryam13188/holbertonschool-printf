cb71a1a595b554ec1fd662e639db4b82a59ff9ad
maryam13188 <roro13188@gmail.com>
2025-11-19 06:25:49 +0000

    feat: Add precision support and length modifiers

--- main.h
+++ main.h
@@ -4,13 +4,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define BUFFER_SIZE 1024
-
-/* Flag characters */
-#define FLAG_PLUS   1
-#define FLAG_SPACE  2
-#define FLAG_HASH   4
 
 /* Length modifiers */
 #define LENGTH_NONE 0
@@ -19,17 +15,13 @@
 
 typedef struct format_info
 {
-    int flags;
     int length;
+    int precision;
+    int has_precision;
 } format_info_t;
 
 /* Main printf function */
 int _printf(const char *format, ...);
-
-/* Buffer management functions */
-int flush_buffer(char buffer[], int *buff_ind);
-int buffer_char(char c, char buffer[], int *buff_ind);
-int buffer_string(char *str, char buffer[], int *buff_ind);
 
 /* Conversion specifier functions */
 int print_char(va_list args, char buffer[], int *buff_ind, format_info_t info);
@@ -39,15 +31,11 @@ int print_pointer(va_list args, char buffer[], int *buff_ind, format_info_t info
 int print_percent(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_int(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_number(long n, char buffer[], int *buff_ind, format_info_t info);
+int print_number_precision(long n, char buffer[], int *buff_ind, format_info_t info);
 int print_binary(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_binary_recursive(unsigned int n, char buffer[], int *buff_ind, format_info_t info); /* Added prototype */
 int print_unsigned(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_octal(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_hex_lower(va_list args, char buffer[], int *buff_ind, format_info_t info);
 int print_hex_upper(va_list args, char buffer[], int *buff_ind, format_info_t info);
-int print_unsigned_number(unsigned long n, unsigned int base, const char *digits, char buffer[], int *buff_ind, format_info_t info);
-
-/* Helper functions */
-int handle_specifier(const char *format, int *i, va_list args, char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
